e3434649062d828a276c19225a127dd4982e36f4
Denis Boyda <boyda_d@mail.ru>
2018-09-04 11:40:04 +1000

    Empty

--- include/IShower.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-struct IShower{
-  virtual void show(const Board& board) = 0;
-  virtual ~IShower() = default;
-};
--- include/board.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <array>
-
-
-namespace{
-  const size_t boardSize = 1000;
-}
-
-using Board = std::array< std::array<int, boardSize> , boardSize>;
